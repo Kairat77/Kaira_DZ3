06741a253b28873fc98103f80243c713cec54089
kairat <63288004+Kairat77@users.noreply.github.com>
2022-12-17 01:29:38 +0600

    first commit

--- src/tovari/tovari.jsx
+++ src/tovari/tovari.jsx
@@ -3,11 +3,9 @@ import ProductList from "../productList/productList"
 function Tovari() {
     return(
         <>
-
         <h2> Cars </h2>
         <ProductList/>
         </>
     )
 }
-
 export default Tovari;
